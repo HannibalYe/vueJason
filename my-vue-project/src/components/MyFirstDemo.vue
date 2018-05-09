4b55cffe7ec062191d1127f999986adc41ee5b02
jason.zhenchi.ye <jason.zhenchi.ye@accenture.com>
2018-05-09 16:08:39 +0800

    update vue

---
+++ my-vue-project/src/components/MyFirstDemo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="myfirst">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+ 
+<script>
+export default {
+  name: 'myfirst',
+  data () {
+    return {
+      msg: '欢迎来到菜鸟教程！'
+    }
+  }
+}
+</script>
