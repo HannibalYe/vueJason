d4a9f849a24837b933f85a6f2736b894932a5a20
Ye <jason.zhenchi.ye@accenture.com>
2018-05-23 13:18:39 +0800

    update

--- my-vue-project2/src/main.js
+++ my-vue-project2/src/main.js
@@ -1,26 +1,28 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+//import App from './App'
+import Layout from './components/layout'
 import $ from 'jquery'
 import Vuex from 'vuex'
-
+import VueRouter from 'vue-router'
+import IndexPage from './pages/index'
+Vue.use(VueRouter)
 let router = new VueRouter({
 	mode:'history',
 	routes:[
 	{
-		path:'/apple'
-		//component:Apple
+		path:'/',
+		component:IndexPage
 	}
 	]
 });
 
-Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  components: { Layout },
+  template: '<Layout/>'
 })
